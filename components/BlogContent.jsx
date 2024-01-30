bdf98fae8b123ebfb64098c2e2721e8ec9c336a6
williamqin14 <84489685+williamqin14@users.noreply.github.com>
2024-01-30 00:55:05 -0800

    removes blog page

--- components/BlogContent.jsx
+++
@@ -1,7 +0,0 @@
-import DOMPurify from "isomorphic-dompurify" 
-
-export default function BlogContent({ htmlContent }) {
-  const sanitizedHTML = DOMPurify.sanitize(htmlContent);
-
-  return <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />;
-}
