5a295c3a84a78bed451adf0f197a69b9bcb45cc1
williamqin14 <84489685+williamqin14@users.noreply.github.com>
2024-01-30 13:36:13 -0800

    adds decap cms

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (cfg) => {
+    cfg.module.rules.push(
+      {
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        options: { mode: ['react-component'] }
+      }
+    )
+    return cfg
+  }
 };
 
 module.exports = nextConfig
