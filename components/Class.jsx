54d687c4077612b61f4f20fa4ed60aa749fb1fb3
williamqin14 <84489685+williamqin14@users.noreply.github.com>
2024-01-31 01:04:39 -0800

    adds default fields

--- components/Class.jsx
+++ components/Class.jsx
@@ -4,16 +4,16 @@ export default function Class({ prop }) {
     return (
         <div className='flex flex-col py-5 text-center border-4 border-black my-3'>
             <div className='text-2xl font-bold uppercase'>{prop.class_title}</div>
-            <div>{prop.class_description}</div>
-            {/* {prop.classes.map((c, k) => (
-              <div key={k} className='flex flex-row justify-center gap-4'>
-                <div>{c.level}</div>
-                <div>{c.ages}</div>
-                <div>{c.day}</div>
-                <div>{c.day_range}</div>
-                <div>{c.time_range}</div>
+            <Markdown>{prop.class_description}</Markdown>
+            {prop.classes?.map((c, k) => (
+              <div key={k} className='flex flex-row text-sm sm:text-base justify-center md:px-20 lg:px-48 xl:px-72 2xl:px-[25%]'>
+                <div className='flex-1'>{c.level}</div>
+                <div className='flex-1'>{c.ages}</div>
+                <div className='flex-1'>{c.day}</div>
+                <div className='flex-1'>{c.day_range}</div>
+                <div className='flex-1'>{c.time_range}</div>
               </div>
-            ))} */}
+            ))}
             <div className='flex flex-row justify-center gap-4'>
                 <div><strong>Class Fee: </strong>{prop.class_fee}</div>
                 <div><strong>Material Fee: </strong>{prop.material_fee}</div>
