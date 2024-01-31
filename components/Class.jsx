fe204db9915d3d75eea0599fc8ee0c04c0214970
williamqin14 <84489685+williamqin14@users.noreply.github.com>
2024-01-31 00:41:05 -0800

    better class display

--- components/Class.jsx
+++ components/Class.jsx
@@ -5,7 +5,7 @@ export default function Class({ prop }) {
         <div className='flex flex-col py-5 text-center border-4 border-black my-3'>
             <div className='text-2xl font-bold uppercase'>{prop.class_title}</div>
             <div>{prop.class_description}</div>
-            {prop.classes.map((c, k) => (
+            {/* {prop.classes.map((c, k) => (
               <div key={k} className='flex flex-row justify-center gap-4'>
                 <div>{c.level}</div>
                 <div>{c.ages}</div>
@@ -13,7 +13,7 @@ export default function Class({ prop }) {
                 <div>{c.day_range}</div>
                 <div>{c.time_range}</div>
               </div>
-            ))}
+            ))} */}
             <div className='flex flex-row justify-center gap-4'>
                 <div><strong>Class Fee: </strong>{prop.class_fee}</div>
                 <div><strong>Material Fee: </strong>{prop.material_fee}</div>
