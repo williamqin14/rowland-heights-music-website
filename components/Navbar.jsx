416cc2e06c3b434fd596b44fd8aa1e0f44a59f20
williamqin14 <84489685+williamqin14@users.noreply.github.com>
2024-01-31 16:57:03 -0800

    iphone 14 pro fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -56,7 +56,7 @@ export default function Navbar() {
       <div className={menuIcon ? 
         'md:hidden absolute top-[80px] right-0 flex justify-center items-center w-full h-[350px] bg-gray-900 text-white ease-in duration-250'
         :
-        'md:hidden absolute top-[-100%] right-0 flex justify-center items-center w-full h-[350px] bg-gray-900 text-white text-center ease-in duration-250 mb-[80px]'
+        'md:hidden absolute top-[-150%] right-0 flex justify-center items-center w-full h-[350px] bg-gray-900 text-white text-center ease-in duration-250'
       }>
         <div className='w-full'>
           
